a5e7517c06297add863e7b3debbba1c45b0b7ada
MartinFaeh <121894103+Ristafan@users.noreply.github.com>
2026-06-28 21:36:46 +0200

    Feat: Base Scraper

--- src/Scraper.py
+++ src/Scraper.py
@@ -1,5 +1,6 @@
 import os
 import json
+import requests
 
 
 # Import desired job configurations from the jobs.json file
@@ -20,6 +21,37 @@ def import_job_configs(num_priorities: int) -> list:
     # Return the specified number of priority levels
     return job_configs[:num_priorities]
 
+
+class Scraper:
+    def __init__(self, job_configs: list):
+        self.job_configs = job_configs
+        self.found_jobs = []
+
+    def scrape_jobs(self):
+        for job in self.job_configs:
+            url = job['url']
+            api_endpoint = job.get('apiEndpoint', '')
+            needs_special_instructions = job.get('needsSpecialInstructions', False)
+            special_instructions = job.get('specialInstructions', '')
+
+            if needs_special_instructions:
+                self.prepare_special_instructions(special_instructions)
+
+            # Fetch web content (this is a placeholder for actual scraping logic)
+            self.get_web_content(url + api_endpoint)
+
+    
+    def get_job_names(self):
+        job_names = [job['name'] for job in self.job_configs]
+        return job_names
+    
+    def get_web_content(self):
+        # Placeholder for fetching web content
+        print(f"Fetching content from {url}")
+        # Here you would implement the actual logic to fetch web content
+
+    def prepare_special_instructions(self):
+        pass
 
 
 
